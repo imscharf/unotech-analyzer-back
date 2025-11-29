d2b79d98a3a81f878687f9d34542f4e901ee3379
João Pedro Scharf <jpscharf@hotmail.com>
2025-11-29 17:43:21 -0300

    Update apiprojeto.py

--- API/apiprojeto.py
+++ API/apiprojeto.py
@@ -17,6 +17,7 @@ from fastapi.middleware.cors import CORSMiddleware
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://unotech-analyzer.vercel.app/"
 ]
 
 app = FastAPI(
